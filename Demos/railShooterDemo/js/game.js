2b4c05219e11cbe9a92030fbfed7936643444b64
Jarretier Adrien <jarretier.adrien@gmail.com>
2015-02-18 23:49:15 +0100

    Added Game.createTerrain() : create a Plane and some cubes on the surface Added Shadows from directional light in Game.add_elements() (should we use Game.addLight() for light ? ) Removed some forgotten lines related to previou random cubes generation

--- Demos/railShooterDemo/js/game.js
+++ Demos/railShooterDemo/js/game.js
@@ -15,8 +15,14 @@ Game.init = function () {
     Game.scene = new THREE.Scene();
 
     Game.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 15000);
-    Game.camera.position.set(0, 10, 0);
+    Game.camera.position.set(0, 30, 200);
+   // Game.camera.lookAt(new THREE.Vector3(0,0,0));
     Game.scene.add(Game.camera);
+
+    Game.renderer.shadowMapEnabled = true;
+    Game.renderer.shadowMapSoft = true;
+
+    Game.createTerrain();
 
 /*    Game.controls;
     if(Game.isdisplayedOn3D){
@@ -59,37 +65,77 @@ Game.init = function () {
       Game.add_elements();
 
       window.addEventListener('resize', Game.resize, false);
-      setTimeout(Game.resize, 1);	
+      setTimeout(Game.resize, 1);   
+}
+
+Game.createTerrain = function()
+{
+    var terrainWidth = 1024;
+    var boxWidth = 5;
+    var density = 1;
+    var planeGeometry = new THREE.PlaneGeometry(terrainWidth, terrainWidth);
+    var material = new THREE.MeshPhongMaterial( { ambient: 0x333333, color: 0xffffff, specular: 0xffffff, shininess: 50 } );
+
+    var terrainMesh = new THREE.Mesh(planeGeometry, material);
+
+    terrainMesh.receiveShadow = true;
+
+    terrainMesh.rotation.x = THREE.Math.degToRad(-90);
+    terrainMesh.updateMatrix();
+
+    for(var i=0; i<terrainWidth*terrainWidth*density/(100*boxWidth*boxWidth); i++)
+    {
+        var boxGeometry = new THREE.BoxGeometry(boxWidth, boxWidth, boxWidth);
+        var boxMaterial = new THREE.MeshPhongMaterial( { ambient: '#'+Math.floor(Math.random()*16777215).toString(16)
+, color: 0xffffff, specular: 0xffffff, shininess: 50 } );
+
+        var boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+        boxMesh.position.x = terrainWidth/2 * ( 2.0 * Math.random() - 1.0 );
+        boxMesh.position.z = terrainWidth/2 * ( 2.0 * Math.random() - 1.0 );
+        boxMesh.position.y = boxWidth/2;
+
+        boxMesh.castShadow = true;
+
+        boxMesh.updateMatrix();
+
+        boxMesh.matrixAutoUpdate = false;
+
+        Game.scene.add(boxMesh);
+    }
+
+   // mesh.rotation. = ;
+
+    terrainMesh.matrixAutoUpdate = false;
+
+    Game.scene.add( terrainMesh );
+
+    // console.log("terrain");
 }
 
 Game.add_elements = function ()
 {
-  var ambient = new THREE.AmbientLight( 0xffffff );
-  ambient.color.setHSL( 0.1, 0.3, 0.2 );
-  Game.scene.add( ambient );
-
-
-  var dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
-  dirLight.position.set( 0, -1, 0 ).normalize();
-  Game.scene.add( dirLight );
-
-  dirLight.color.setHSL( 0.1, 0.7, 0.5 );
-
-  var s = 250;
-
-  var cube = new THREE.BoxGeometry( s, s, s );
-  var material = new THREE.MeshPhongMaterial( { ambient: 0x333333, color: 0xffffff, specular: 0xffffff, shininess: 50 } );
-
     var ambient = new THREE.AmbientLight( 0xffffff );
-    ambient.color.setHSL( 0.1, 0.3, 0.2 );
+    ambient.color.setHSL( 0.1, 0.3, 0.4 );
     Game.scene.add( ambient );
 
 
-    var dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
-    dirLight.position.set( 0, -1, 0 ).normalize();
+    var dirLight = new THREE.DirectionalLight( 0xffffff, 0.200 );
+    dirLight.position.set( 512, 1024, 0 );
+    dirLight.color.setHSL( 0.1, 0.7, 1 );
+    dirLight.target.position.set(0, 0, 0);
+    dirLight.castShadow = true;
+    //dirLight.shadowCameraVisible = true; // only for debugging
+    dirLight.shadowMapWidth = 4096;
+    dirLight.shadowMapHeight = dirLight.shadowMapWidth;
+
     Game.scene.add( dirLight );
 
-    dirLight.color.setHSL( 0.1, 0.7, 0.5 );
+    var dirLightLeft = new THREE.DirectionalLight( 0xffffff, 0.125 );
+    dirLightLeft.position.set( -10, 0, 0 );
+    dirLightLeft.color.setHSL( 0.1, 0.7, 1 );
+
+    Game.scene.add( dirLightLeft );
 
     /*Game.textureFlare0 = THREE.ImageUtils.loadTexture( "textures/lensflare0.png" );
     Game.textureFlare2 = THREE.ImageUtils.loadTexture( "textures/lensflare2.png" );
@@ -128,22 +174,22 @@ Game.addLight = function ( h, s, l, x, y, z ) {
 
 Game.resize = function ()
 {
-	var width = Game.container.offsetWidth;
-	var height = Game.container.offsetHeight;
+    var width = Game.container.offsetWidth;
+    var height = Game.container.offsetHeight;
 
-	Game.camera.aspect = width / height;
-	Game.camera.updateProjectionMatrix();
+    Game.camera.aspect = width / height;
+    Game.camera.updateProjectionMatrix();
 
-	Game.renderer.setSize(width, height);
+    Game.renderer.setSize(width, height);
     if(Game.isdisplayedOn3D) {
-	   Game.effect.setSize(width, height);
+       Game.effect.setSize(width, height);
     }
 }
 
 
 Game.update = function (dt) 
 {
-	Game.resize();
+    Game.resize();
 
     Game.camera.updateProjectionMatrix();
 
@@ -160,7 +206,7 @@ Game.update = function (dt)
 }
 
 Game.onWindowResize = function( event ) {
-	var width = Game.container.offsetWidth;
+    var width = Game.container.offsetWidth;
     var height = Game.container.offsetHeight;
 
     Game.camera.aspect = width / height;
@@ -175,10 +221,10 @@ Game.onWindowResize = function( event ) {
 //
 
 Game.animate = function() {
- 	requestAnimationFrame(Game.animate);
+    requestAnimationFrame(Game.animate);
       Game.update(Game.clock.getDelta());
       Game.render(Game.clock.getDelta());
-	
+    
 }
 
 Game.render = function () {
