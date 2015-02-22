e18b7264648a5ffd41b5dbce4fa22b9dd113a3fb
guillaume_gomez <guillaume.gomez846@gmail.com>
2015-02-22 18:45:49 +0000

    add two static variables

--- Demos/railShooterDemo/js/enemy.js
+++ Demos/railShooterDemo/js/enemy.js
@@ -1,11 +1,16 @@
 
+
+
 var Enemy = function (vector3Pos, radius) {
 
-	var geometry = new THREE.SphereGeometry( this, radius, 32, 32);
-	var material2 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 	THREE.Mesh.call(this, geometry, material2);
 
 }
+
+// static variables 
+Enemy.geometry = new THREE.SphereGeometry( this, radius, 32, 32);
+Enemy.material2 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+
 
 Enemy.prototype = Object.create( THREE.Mesh.prototype );
 
