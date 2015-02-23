ff7dee9045883797a0203f096717f481d4a71d84
guillaume-gomez <guillaume.gomez846@gmail.com>
2015-02-23 08:51:56 +0100

    class Enemy is working now

--- Demos/railShooterDemo/js/enemy.js
+++ Demos/railShooterDemo/js/enemy.js
@@ -1,18 +1,19 @@
-
-
-
-var Enemy = function (vector3Pos, radius) {
-
-	THREE.Mesh.call(this, geometry, material2);
-
+var Enemy = function (vector3Pos, geom, mate) {
+	console.log("new Enemy("+vector3Pos.x+", "+vector3Pos.y+", "+vector3Pos.z+")");
+	THREE.Mesh.call(this, Enemy.geometry, Enemy.material );
+	this.position.copy(vector3Pos);
+	console.log( this.position );
 }
 
-// static variables 
-Enemy.geometry = new THREE.SphereGeometry( this, radius, 32, 32);
-Enemy.material2 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-
+// static properties 
+Enemy.geometry = new THREE.SphereGeometry(2, 32, 32);
+Enemy.material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+Enemy.castShadow = true;
+Enemy.receiveShadow = true;
+Enemy.type = "Enemy";
 
 Enemy.prototype = Object.create( THREE.Mesh.prototype );
+Enemy.prototype.constructor = Enemy;
 
 Enemy.prototype.move = function () {
 
