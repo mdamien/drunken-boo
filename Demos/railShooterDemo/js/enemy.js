6a4d8d0b62218a0ee6375181b2082d7d2433b4aa
guillaume-gomez <guillaume.gomez846@gmail.com>
2015-02-23 23:09:57 +0100

    add a properties from THREE.Object. It will be useful to only delete enemies thanks to this property

--- Demos/railShooterDemo/js/enemy.js
+++ Demos/railShooterDemo/js/enemy.js
@@ -1,8 +1,8 @@
 var Enemy = function (vector3Pos, geom, mate) {
-	console.log("new Enemy("+vector3Pos.x+", "+vector3Pos.y+", "+vector3Pos.z+")");
+	//console.log("new Enemy("+vector3Pos.x+", "+vector3Pos.y+", "+vector3Pos.z+")");
 	THREE.Mesh.call(this, Enemy.geometry, Enemy.material );
 	this.position.copy(vector3Pos);
-	console.log( this.position );
+	//console.log( this.position );
 }
 
 // static properties 
@@ -11,6 +11,7 @@ Enemy.material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
 Enemy.castShadow = true;
 Enemy.receiveShadow = true;
 Enemy.type = "Enemy";
+Enemy.name = "Enemy";
 
 Enemy.prototype = Object.create( THREE.Mesh.prototype );
 Enemy.prototype.constructor = Enemy;
